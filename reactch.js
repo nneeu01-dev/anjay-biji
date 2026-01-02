1e3f1fd37ecb120ef16d88d388c7ed505f12cd01
nneeu01-dev <nneeu01@gmail.com>
2026-01-02 18:56:03 +0700

    Create reactch.js

---
+++ reactch.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  const { link, emoji } = req.query
+
+  if (!link || !emoji) {
+    return res.status(400).json({ error: "link & emoji wajib" })
+  }
+
+  try {
+    const apiRes = await fetch(
+      `https://react.whyux-xec.my.id/api/rch?link=${encodeURIComponent(link)}&emoji=${encodeURIComponent(emoji)}`,
+      {
+        headers: {
+          "x-api-key": "d08ab503b161c0fc400041c7b2b955ae6b6e9dd08e654adbee321d0d59d45227"
+        }
+      }
+    )
+
+    const data = await apiRes.json()
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json({ error: "Proxy error" })
+  }
+}
